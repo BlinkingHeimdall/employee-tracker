8abc0f1eb0ee809a118cb870b40cf3de3ca7aea5
Jason Adams <jmadams41@gmail.com>
2022-06-05 18:52:25 -0600

    added view all departments and set up schema

--- app.js
+++ app.js
@@ -9,32 +9,35 @@ function init() {
                 type: 'list',
                 name: 'action',
                 message: 'What would you like to do?',
-                choices: ['all roles', 'all employees', 'add department', 'add role', 'add employee', 'update employee role']
+                choices: ['all departments','all roles', 'all employees', 'add department', 'add role', 'add employee', 'update employee role']
             },
         ]). then (input => {
             switch(input.action) {
+                case 'all departments':
+                    viewDepartments();
+                    break;
                 case 'all roles':
                     viewRoles();
                     break;
-                    case 'view all employees':
-                        viewEmployees();
-                        break;
-                    case 'add a department':
-                        addDepartment();
-                        break;
-                    case 'add a role':
-                        addRole();
-                        break;
-                    case 'add an employee':
-                        addEmployee();
-                        break;
-                    case 'update an employee role':
-                        updateRole();
-                        break;
+                case 'all employees':
+                    viewEmployees();
+                    break;
+                case 'add department':
+                    addDepartment();
+                    break;
+                case 'add role':
+                    addRole();
+                    break;
+                case 'add employee':
+                    addEmployee();
+                    break;
+                case 'update employee role':
+                    updateRole();
+                    break;
                 };
             });
         };
-    
+        
         function restart() {
             inquirer.prompt([
                 {
@@ -50,7 +53,20 @@ function init() {
                 }
             });
         };
-        
+
+        function viewDepartments() {
+            const sql = `SELECT * FROM departments`;
+
+            db.query(sql, (err, rows) => {
+                if(err) {
+                    console.log(err.message);
+                    return;
+                }
+                console.table(rows);
+                restart();
+            });
+        };
+
         function viewRoles() {
             const sql = `SELAECT * From roles`;
             db.query(sql, (err, rows) => {
@@ -64,12 +80,36 @@ function init() {
         };
     
         function viewEmployees() {
-    
+                const sql = `SELECT * FROM employees`;
+                db.query(sql, (err, rows) => {
+                    if (err) {
+                        console.log(err.message)
+                        return;
+                    }
+                    console.table(rows);
+                    restart();
+                });
         };
     
         function addDepartment() {
-    
-        };
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'new-department',
+                    message: 'What is the new department called'
+                }
+            ]).then(input => {
+                const sql = `INSERT INTO departments (title) VALUES (?)`;
+                const params = input.new_department;
+                db.query(sql, params, (err, result) => {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+                    viewDepartments();
+                });
+            });
+        };;
     
         function addRole() {
     
---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
+
+CREATE TABLE departments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INT,
+    CONSTRAINT fk_department FOREIGN KEY(department_id) REFERENCES departments(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    salary_id INT,
+    role_id INT,
+    manager_id INT,
+    CONSTRAINT fk_role_id FOREIGN KEY(role_id) REFERENCES roles(id) ON DELETE SET NULL
